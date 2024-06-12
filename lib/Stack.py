7a5a6293df2f99fb8427b27d5bb808e0db047554
l3xst1ng <howdy.martt@gmail.com>
2024-06-12 17:21:26 +0300

    Completed python-p3-dsa-stack-lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,50 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
+        
 
     def isEmpty(self):
-        pass
-
-    def push(self, item):
-        pass
-
-    def pop(self):
-        pass
-
-    def peek(self):
-        pass
+        return len(self.items) == 0
     
-    def size(self):
-        pass
+
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
+        
+
+    def push(self, item):
+        if not self.full():
+            self.items.append(item)
+        else:
+            return None
+    
+        
+        
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
+            
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
+            
+    
+    def size(self):
+        return len(self.items)
+    
 
     def search(self, target):
-        pass
+        for i in reversed(range(len(self.items))):
+            if self.items[i] == target:
+                return len(self.items)-1- i
+        return -1
